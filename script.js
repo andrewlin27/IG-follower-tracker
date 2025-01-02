35ab92320a9570a3127aab87040479cc37bafa93
Andrew Lin <andrew.mlin27@gmail.com>
2025-01-02 03:49:28 -0600

    mobile styling

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ document.getElementById('fileInput').addEventListener('change', async (event) =>
 
         const followersJSON = JSON.parse(await followersFile.async('string'));
         const followingJSON = JSON.parse(await followingFile.async('string')).relationships_following;
-        
+
         const followers = new Set(followersJSON.map(f => f.string_list_data[0].value));
         const following = new Set(followingJSON.map(f => f.string_list_data[0].value));
 
@@ -26,19 +26,25 @@ document.getElementById('fileInput').addEventListener('change', async (event) =>
         const notFollowedBack = Array.from(followers).filter(user => !following.has(user));
 
         resultsDiv.innerHTML = `
-            <div class="result">
-                <h3>Stats</h3>
-                <p><strong>People not following you back:</strong> ${notFollowingBack.length}</p>
-                <p><strong>People you don't follow back:</strong> ${notFollowedBack.length}</p>
+        <div class="numbers">
+            <h3>Stats</h3>
+            <p><strong>People not following you back:</strong> ${notFollowingBack.length}</p>
+            <p><strong>People you don't follow back:</strong> ${notFollowedBack.length}</p>
+        </div>
+        <div class="result">
+            <h3>Details</h3>
+            <div class="details">
+                <div>
+                    <h4>Not following you back:</h4>
+                    <ul>${notFollowingBack.map(user => `<li>${user}</li>`).join('')}</ul>
+                </div>
+                <div>
+                    <h4>You don't follow back:</h4>
+                    <ul>${notFollowedBack.map(user => `<li>${user}</li>`).join('')}</ul>
+                </div>
             </div>
-            <div class="result">
-                <h3>Details</h3>
-                <p><strong>Not following you back:</strong></p>
-                <ul>${notFollowingBack.map(user => `<li>${user}</li>`).join('')}</ul>
-                <p><strong>You don't follow back:</strong></p>
-                <ul>${notFollowedBack.map(user => `<li>${user}</li>`).join('')}</ul>
-            </div>
-        `;
+        </div>
+    `;
     } catch (err) {
         resultsDiv.innerHTML = `<p>Error: ${err.message}</p>`;
     }
